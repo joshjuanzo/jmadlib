dd34105dd1dc9fba3c0e912e7139c7c7399e80a7
joshjuanzo <164358064+joshjuanzo@users.noreply.github.com>
2024-04-25 21:04:13 +0000

    updated index.html and script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+  const firebaseConfig = {
     apiKey: "AIzaSyDWm7CFaAQ6mS32fOk1ITe09l1URRW1DH8",
     authDomain: "madlibs-2a935.firebaseapp.com",
     projectId: "madlibs-2a935",
@@ -7,52 +7,117 @@ const firebaseConfig = {
     appId: "1:130267669017:web:5db8e9365603966eaf8088"
   };
 
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-console.log("firebase setup complete!");
+  const app = firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+  console.log("Firebase setup complete!");
 
-function saveMadlib(){
-  console.log("saveMadlib() called");
+  function createMadLib() {
+    console.log("createMadLib() called");
+    var noun_person = document.getElementById('noun_person').value;
+    var noun_place = document.getElementById('noun_place').value;
+    var adjective = document.getElementById('adjective').value;
+    var noun_things_plural = document.getElementById('noun_things_plural').value;
+    var interjection = document.getElementById('interjection').value;
+    var preposition = document.getElementById('preposition').value;
+    var conjunction = document.getElementById('conjunction').value;
+    var pronoun = document.getElementById('pronoun').value;
+    var verb = document.getElementById('verb').value;
+    var adverb = document.getElementById('adverb').value;
 
-var outputData = createMadlib();
-db.collection("madlibs").doc(outputData.outputName).set(outputData);
-alert(outputData.outputName + " saved to database!");
-}
+    var storyName = document.getElementById("outputData").value;
 
-function printMadLib() {
-  console.log("printMadLib() called");
-  var noun_person = document.getElementById('noun_person').value;
-  var noun_place = document.getElementById('noun_place').value;
-  var adjective = document.getElementById('adjective').value;
-  var noun_things_plural = document.getElementById('noun_things_plural').value;
-  var interjection = document.getElementById('interjection').value;
-  var preposition = document.getElementById('preposition').value;
-  var conjunction = document.getElementById('conjunction').value;
-  var pronoun = document.getElementById('pronoun').value;
-  var verb = document.getElementById('verb').value;
-  var adverb = document.getElementById('adverb').value;
+    var storyHTML = "The realm of <span class='noun'>" + noun_place + "</span> was a happy place until the evil knight, Sir <span class='adjective'>" + adjective + "</span> <span class='noun'>" + noun_things_plural + "</span>, stole the king's prized horse. '<span class='interjection'>" + interjection + "</span>! That's <span class='pronoun'>" + pronoun + "</span>,' yelled the king as the evil knight fled. The king gathered all of his townspeople and announced, 'Someone stole my horse and I know I won't feel better <span class='conjunction'>" + conjunction + "</span> horses <span class='verb'>" + verb + "</span> <span class='adverb'>" + adverb + "</span>. Who dares to fight this knight?' The crowd was silent. Then a young child named <span class='noun'>" + noun_person + "</span> stepped forward and said, 'I will!'. The child left before dusk, and in the morning, everyone was surprised because the child returned safely while riding the king's prized horse. 'The child did it,' the town cheered. There was much rejoicing <span class='preposition'>" + preposition + "</span> the horses. The end." + storyName;
 
-  document.getElementById('output').innerHTML = "The realm of <span class='noun'>" + noun_place + "</span> was a happy place until the evil knight, Sir <span class='adjective'>" + adjective + "</span> <span class='noun'>" + noun_things_plural + "</span>, stole the king's prized horse. '<span class='interjection'>" + interjection + "</span>! That's <span class='pronoun'>" + pronoun + "</span>,' yelled the king as the evil knight fled. The king gathered all of his townspeople and announced, 'Someone stole my horse and I know I won't feel better <span class='conjunction'>" + conjunction + "</span> horses <span class='verb'>" + verb + "</span> <span class='adverb'>" + adverb + "</span>. Who dares to fight this knight?' The crowd was silent. Then a young child named <span class='noun'>" + noun_person + "</span> stepped forward and said, 'I will!'. The child left before dusk, and in the morning, everyone was surprised because the child returned safely while riding the king's prized horse. 'The child did it,' the town cheered. There was much rejoicing <span class='preposition'>" + preposition + "</span> the horses. The end."; 
-var output = document.getElementById("output").innerHTML;
-console.log("story: " + output);
+    document.getElementById("story").innerHTML = storyHTML;
 
-  var outputData = {
-    timestamp: Date.now(),
-    story: output,
-    noun_person: noun_person,
-    noun_place: noun_place,
-    adjective: adjective,
-    noun_things_plural: noun_things_plural,
-    interjection: interjection,
-    preposition: preposition,
-    conjunction: conjunction,
-    pronoun: pronoun,
-    verb: verb,
-    adverb: adverb,
-};
+    var storyData = {
+      timestamp: Date.now(),
+      story: storyHTML,
+      noun_person: noun_person,
+      noun_place: noun_place,
+      adjective: adjective,
+      noun_things_plural: noun_things_plural,
+      interjection: interjection,
+      preposition: preposition,
+      conjunction: conjunction,
+      pronoun: pronoun,
+      verb: verb,
+      adverb: adverb,
+      storyName: storyName,
+    };
 
-var outputJSON = JSON.stringify(outputData)
-console.log("outputJSON: "+ outputJSON);
-return outputData;
+    var storyJSON = JSON.stringify(storyData);
+    console.log("storyJSON: " + storyJSON);
+
+    return storyData;
   }
-  
+
+  function saveMadlib() {
+    console.log("saveMadlib() called");
+
+    var storyData = createMadLib();
+    db.collection("madlibs").doc(storyData.storyName).set(storyData)
+      .then(() => {
+        alert(storyData.storyName + " saved to database!");
+      })
+      .catch((error) => {
+        console.error("Error saving document: ", error);
+        alert("Failed to save madlib!");
+      });
+  }
+
+  function retrieveMadlib() {
+    console.log("retrieveMadlib() called");
+
+    var storyName = prompt("Enter the name of the story you want to look up:");
+    db.collection("madlibs").doc(storyName).get()
+      .then((doc) => {
+        if (doc.exists) {
+          console.log("Document data:", doc.data());
+          var storyData = doc.data();
+
+          document.getElementById("noun_person").value = storyData.noun_person;
+          document.getElementById("noun_place").value = storyData.noun_place;
+          document.getElementById("adjective").value = storyData.adjective;
+          document.getElementById("noun_things_plural").value = storyData.noun_things_plural;
+          document.getElementById("interjection").value = storyData.interjection;
+          document.getElementById("preposition").value = storyData.preposition;
+          document.getElementById("conjunction").value = storyData.conjunction;
+          document.getElementById("pronoun").value = storyData.pronoun;
+          document.getElementById("verb").value = storyData.verb;
+          document.getElementById("adverb").value = storyData.adverb;
+
+          document.getElementById("outputData").value = storyData.storyName;
+
+          document.getElementById("story").innerHTML = storyData.story;
+        } else {
+          console.log("No such document!");
+          document.getElementById("story").innerHTML = "Story not found!";
+        }
+      })
+      .catch((error) => {
+        console.error("Error getting document:", error);
+        document.getElementById("story").innerHTML = "Error retrieving story!";
+      });
+  }
+
+  function deleteMadLib() {
+    console.log("deleteMadLib() called");
+    var storyName = prompt("Enter the name of the story you want to delete:");
+    db.collection("madlibs").doc(storyName).get()
+      .then((doc) => {
+        if (doc.exists) {
+          console.log("Document data:", doc.data());
+          var storyData = doc.data();
+          document.getElementById("story").innerHTML = storyData.storyName + " successfully deleted!";
+          db.collection("madlibs").doc(storyName).delete();
+        } else {
+          console.log("No such document!");
+          document.getElementById("story").innerHTML = "Story not found!";
+        }
+      })
+      .catch((error) => {
+        console.error("Error deleting document:", error);
+        document.getElementById("story").innerHTML = "Error deleting story!";
+      });
+  }
